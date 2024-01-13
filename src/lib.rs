b35df66380932d30b3374f18982e031b3c6aed24
xuxiaocheng <2762267080@qq.com>
2024-01-13 11:27:34 +0800

    feat(Lib): 使用ureq，减少依赖

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,17 @@ use std::collections::HashMap;
 use std::str::FromStr;
 use std::time::Duration;
 use anyhow::{anyhow, Result};
-use reqwest::{ClientBuilder, IntoUrl};
+
+pub extern crate ureq;
 
 fn get_field<T: FromStr>(map: &HashMap<String, String>, key: &str) -> Result<T> {
     Ok(map.get(key).ok_or_else(|| anyhow!("Missing field: {}", key))?.parse()?)
 }
 
-pub async fn report<U: IntoUrl>(timeout: Duration, url: U, name: &str, version: &str) -> Result<bool> {
-    let client = ClientBuilder::new().timeout(timeout).build()?;
-    let mut body = HashMap::new();
-    body.insert("crate", name);
-    body.insert("ver", version);
-    let res = client.execute(client.post(url).json(&body).build()?).await?
-        .json::<HashMap<String, String>>().await?;
+pub fn report(url: &str, timeout: Duration, name: &str, version: &str) -> Result<bool> {
+    let res = ureq::post(url).timeout(timeout)
+        .send_json(ureq::json!({"crate": name, "ver": version}))?
+        .into_json::<HashMap<String, String>>()?;
     if get_field::<bool>(&res, "ok")? {
         Ok(get_field::<bool>(&res, "success")?)
     } else {
