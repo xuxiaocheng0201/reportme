4066bb0bfea12243a6e65f032109fccb8c3063cd
xuxiaocheng <2762267080@qq.com>
2024-01-13 13:08:04 +0800

    fix(Lib): 优化返回类型

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,16 @@ fn get_field<T: FromStr>(map: &HashMap<String, String>, key: &str) -> Result<T> 
     Ok(map.get(key).ok_or_else(|| anyhow!("Missing field: {}", key))?.parse()?)
 }
 
-pub fn report(url: &str, timeout: Duration, name: &str, version: &str) -> Result<bool> {
+pub fn report(url: &str, timeout: Duration, name: &str, version: &str) -> Result<()> {
     let res = ureq::post(url).timeout(timeout)
         .send_json(ureq::json!({"crate": name, "ver": version}))?
         .into_json::<HashMap<String, String>>()?;
     if get_field::<bool>(&res, "ok")? {
-        Ok(get_field::<bool>(&res, "success")?)
+        if get_field::<bool>(&res, "success")? {
+            Ok(())
+        } else {
+            Err(anyhow!("Failed to report."))
+        }
     } else {
         let err = get_field::<String>(&res, "err")?;
         Err(if get_field::<bool>(&res, "internal")? {
