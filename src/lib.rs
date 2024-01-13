e2307b9ee0a31693784de28959a03855f7f46971
xuxiaocheng <2762267080@qq.com>
2024-01-13 14:31:19 +0800

    feat(Lib): 优化json读取

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,30 @@
-use std::collections::HashMap;
-use std::error::Error;
-use std::str::FromStr;
 use std::time::Duration;
 use anyhow::{anyhow, Result};
+use ureq::serde_json::Value;
 
-fn get_field<T: FromStr>(map: &HashMap<String, String>, key: &str) -> Result<T> where T::Err: Error + Send + Sync + 'static {
-    Ok(map.get(key).ok_or_else(|| anyhow!("Missing field: {}", key))?.parse()?)
+fn get_bool(json: &Value, key: &str) -> Result<bool> {
+    Ok(json.get(key).ok_or_else(|| anyhow!("Missing field: {}", key))?
+        .as_bool().ok_or_else(|| anyhow!("Except bool."))?)
+}
+
+fn get_str<'a>(json: &'a Value, key: &str) -> Result<&'a str> {
+    Ok(json.get(key).ok_or_else(|| anyhow!("Missing field: {}", key))?
+        .as_str().ok_or_else(|| anyhow!("Except str."))?)
 }
 
 pub fn report(url: &str, timeout: Duration, name: &str, version: &str) -> Result<()> {
     let res = ureq::post(url).timeout(timeout)
         .send_json(ureq::json!({"crate": name, "ver": version}))?
-        .into_json::<HashMap<String, String>>()?;
-    if get_field::<bool>(&res, "ok")? {
-        if get_field::<bool>(&res, "success")? {
+        .into_json::<Value>()?;
+    if get_bool(&res, "ok")? {
+        if get_bool(&res, "success")? {
             Ok(())
         } else {
             Err(anyhow!("Failed to report."))
         }
     } else {
-        let err = get_field::<String>(&res, "err")?;
-        Err(if get_field::<bool>(&res, "internal")? {
+        let err = get_str(&res, "err")?;
+        Err(if get_bool(&res, "internal")? {
             anyhow!("Internal error: {}", err)
         } else {
             anyhow!("Error: {}", err)
